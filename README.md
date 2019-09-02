ed7e66b6e326709b3f676a533357d513f156e9e2
Rootrl <danhoo@163.com>
2019-09-02 17:30:44 +0800

    modified Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Android Samples
 
+## Articles
+
+#### ConstraintLayout
+https://github.com/rootrl/AndroidSamples/blob/b08e520686fb58160a728153dcc5bfaa444c0806/docs/ConstraintLayout.md
+
+#### Memory optimization
+
+https://github.com/rootrl/AndroidSamples/blob/b08e520686fb58160a728153dcc5bfaa444c0806/docs/Memery.md
--- app/src/main/java/com/rootrl/androidsamples/DemoActivity.java
+++ app/src/main/java/com/rootrl/androidsamples/DemoActivity.java
@@ -3,12 +3,20 @@ package com.rootrl.androidsamples;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.VideoView;
 
 public class DemoActivity extends AppCompatActivity {
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
+
+        Log.e("=== DemoActivity =====", "onCreate: " + getPackageResourcePath() );
+
+        // videoView.setVideoPath();
     }
 }
